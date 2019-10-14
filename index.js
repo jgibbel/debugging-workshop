ef778fb7eaf87898af710fd06b9fd7d3c5375225
jgibbel <jamesgibbel@gmail.com>
2019-10-14 10:41:02 -0400

    fetch second arg confusing me, and then in even listener fetch call back

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => jokeData.joke)
   }
 
   form.addEventListener('submit', (event) => {
-
+    event.preventDefault()
+    const username = event.target['name-input'].value
     if(username === "") return;
-    fetchJoke()
+    const newJokeLi = document.createElement("li")
+    fetchJoke().then(joke => {
     newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
     `
     jokeList.appendChild(newJokeLi)
+  })
   })
 })
